3a6a59784ac188eeea4d46647a466e51721ab268
LouisFranz <louis.franz01@gmail.com>
2024-07-27 19:20:20 +0200

    Successful handling of switching between live/offline status and tracking  of new Livestream (hopefully)

--- src/main/java/ChannelThread.java
+++ src/main/java/ChannelThread.java
@@ -1,37 +1,71 @@
 import com.github.kusaanko.youtubelivechat.ChatItem;
+import com.github.kusaanko.youtubelivechat.ChatItemDelete;
 import com.github.kusaanko.youtubelivechat.IdType;
 import com.github.kusaanko.youtubelivechat.YouTubeLiveChat;
+import com.sun.jdi.ThreadReference;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ChannelThread implements Runnable {
 
     String channelName;
     Connection c;
+    int updateTimer;
+    boolean switchedStatus = false;
 
-    public ChannelThread(String channelName, Connection c) {
+    public ChannelThread(String channelName, Connection c, int updateTimer) {
         this.channelName = channelName;
         this.c = c;
+        this.updateTimer = updateTimer;
+    }
+
+    private boolean isLive (String channelID) {
+        try {
+            new YouTubeLiveChat(channelID, true, IdType.CHANNEL);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public void getMessages() throws IOException, InterruptedException, SQLException {
-        String channelId = YouTubeLiveChat.getChannelIdFromURL("https://www.youtube.com/@" +channelName);
-        YouTubeLiveChat chat = new YouTubeLiveChat(channelId, true, IdType.CHANNEL);
 
-        chat.update();
-        Thread.sleep(1000);
+        String channelID = YouTubeLiveChat.getChannelIdFromURL("https://www.youtube.com/@" +channelName);
+        YouTubeLiveChat chat = null;
 
         while (true) {
-            chat.update();
-            for (ChatItem item : chat.getChatItems()) {
-             //   System.out.println(chat.getChannelId() +" " +format.format(new Date(item.getTimestamp() / 1000)) + " " + item.getType() + "[" + item.getAuthorName() + "] " +item.getAuthorChannelID() +" " + item.getAuthorType() + " " + item.getMessage() );
-                mySQLFile.insertData(c, item, channelId, channelName);
+            if(!isLive(channelID)) {
+                switchedStatus = false;
+            //    System.out.println("Channel " +channelName +" is not live.");
+                Thread.sleep(60000);
             }
-            Thread.sleep(1000);
+            else {
+                if(!switchedStatus) {
+                    switchedStatus = true;
+                    chat = new YouTubeLiveChat(channelID, true, IdType.CHANNEL);
+                    chat.update();
+                    Thread.sleep(1000);
+                }
+                else {
+                    try {
+                        chat.update();
+                        for (ChatItem item : chat.getChatItems()) {
+                        //    System.out.println(chat.getChannelId() +" " + mySQLFile.format.format(new Date(item.getTimestamp() / 1000)) + " " + item.getType() + "[" + item.getAuthorName() + "] " +item.getAuthorChannelID() +" " + item.getAuthorType() + " " + item.getMessage() );
+                            mySQLFile.insertData(c, item, channelID, channelName);
+                            mySQLFile.insertDataIDs(c, item);
+                        }
+//            for (ChatItemDelete delete : chat.getChatItemDeletes()) {
+//                System.out.println(delete.getMessage() + " TargetId:" + delete.getTargetId());
+//            }
+                    }  catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Thread.sleep(updateTimer*1000L);
+                }
+            }
         }
     }
 
