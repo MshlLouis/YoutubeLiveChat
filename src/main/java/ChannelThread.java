7c1691405c6f95c7751d8e91246c8dd644d8a2d0
LouisFranz <louis.franz01@gmail.com>
2024-07-27 13:51:55 +0200

    Added support for Database connection

--- src/main/java/ChannelThread.java
+++ src/main/java/ChannelThread.java
@@ -3,56 +3,42 @@ import com.github.kusaanko.youtubelivechat.IdType;
 import com.github.kusaanko.youtubelivechat.YouTubeLiveChat;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ChannelThread implements Runnable {
 
     String channelName;
-    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Connection c;
 
-    public ChannelThread(String channelName) {
+    public ChannelThread(String channelName, Connection c) {
         this.channelName = channelName;
+        this.c = c;
     }
 
-    public void getMessages() throws IOException {
+    public void getMessages() throws IOException, InterruptedException, SQLException {
         String channelId = YouTubeLiveChat.getChannelIdFromURL("https://www.youtube.com/@" +channelName);
         YouTubeLiveChat chat = new YouTubeLiveChat(channelId, true, IdType.CHANNEL);
-        int liveStatusCheckCycle = 0;
 
         chat.update();
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(1000);
 
         while (true) {
             chat.update();
-
             for (ChatItem item : chat.getChatItems()) {
-                System.out.println(chat.getChannelId() +" " +format.format(new Date(item.getTimestamp() / 1000)) + " " + item.getType() + "[" + item.getAuthorName() + "] " +item.getAuthorChannelID() +" " + item.getAuthorType() + " " + item.getMessage() );
+             //   System.out.println(chat.getChannelId() +" " +format.format(new Date(item.getTimestamp() / 1000)) + " " + item.getType() + "[" + item.getAuthorName() + "] " +item.getAuthorChannelID() +" " + item.getAuthorType() + " " + item.getMessage() );
+                mySQLFile.insertData(c, item, channelId, channelName);
             }
-            liveStatusCheckCycle++;
-
-            if(liveStatusCheckCycle >= 60) {
-                if(!chat.getBroadcastInfo().isLiveNow) {
-                    break;
-                }
-                liveStatusCheckCycle = 0;
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Thread.sleep(1000);
         }
     }
 
     public void run() {
         try {
             getMessages();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
