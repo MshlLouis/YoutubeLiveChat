7b408c84f3e0569b26fb8b80acf8db19ae6480c0
LouisFranz <louis.franz01@gmail.com>
2024-07-27 22:38:37 +0200

    Added Support for Paid Messages

--- src/main/java/mySQLFile.java
+++ src/main/java/mySQLFile.java
@@ -19,9 +19,10 @@ public class mySQLFile {
                 " CHANNELID      CHAR(30)   NOT NULL, " +
                 " CHANNELNAME    TEXT       NOT NULL, " +
                 " MESSAGETYPE    TEXT       NOT NULL, " +
+                " PURCHASEAMOUNT TEXT       , " +
                 " AUTHORTYPE     TEXT       NOT NULL, " +
                 " DATE           CHAR(30)   NOT NULL, " +
-                " MESSAGE        TEXT       NOT NULL)" +
+                " MESSAGE        TEXT       )" +
                 " CHARACTER SET  utf8mb4" +
                 " COLLATE        utf8mb4_unicode_520_ci";
         stmt.executeUpdate(sql);
@@ -46,18 +47,24 @@ public class mySQLFile {
         String msgType = String.valueOf(item.getType());
         String authorType = item.getAuthorType().toString();
         String date = format.format(new Date(item.getTimestamp() / 1000));
+        String purchaseAmount = item.getPurchaseAmount();
         String msg = item.getMessage();
 
-        String sql = "INSERT INTO ChatYoutube (USERID,USERNAME,CHANNELID,CHANNELNAME,MESSAGETYPE,AUTHORTYPE,DATE,MESSAGE) VALUES (?,?,?,?,?,?,?,?);";
+        String sql = "INSERT INTO ChatYoutube (USERID,USERNAME,CHANNELID,CHANNELNAME,MESSAGETYPE,PURCHASEAMOUNT,AUTHORTYPE,DATE,MESSAGE) VALUES (?,?,?,?,?,?,?,?,?);";
         try (PreparedStatement pstmt = c.prepareStatement(sql);) {
             pstmt.setString(1, userID);
             pstmt.setBytes(2, userName.getBytes(StandardCharsets.UTF_8));
             pstmt.setString(3, channelID);
             pstmt.setBytes(4, channelName.getBytes(StandardCharsets.UTF_8));
             pstmt.setString(5, msgType);
-            pstmt.setString(6, authorType);
-            pstmt.setString(7, date);
-            pstmt.setBytes(8, msg.getBytes(StandardCharsets.UTF_8));
+            pstmt.setString(6, purchaseAmount);
+            pstmt.setString(7, authorType);
+            pstmt.setString(8, date);
+            if (msg != null) {
+                pstmt.setBytes(9, msg.getBytes(StandardCharsets.UTF_8));
+            } else {
+                pstmt.setString(9, null);
+            }
 
             pstmt.executeUpdate();
         }
@@ -88,7 +95,7 @@ public class mySQLFile {
         mainFile.setCredentials();
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver");
             c = DriverManager
                     .getConnection("jdbc:mysql://"+mainFile.databaseURL,
                             mainFile.databaseUsername,mainFile.databasePassword);
