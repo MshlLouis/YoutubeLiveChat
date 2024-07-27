d2e0cf499bc986cbdf4c5264c762b4236c216899
LouisFranz <louis.franz01@gmail.com>
2024-07-27 13:52:55 +0200

    Added credentials input and Thread Pool

--- src/main/java/mainFile.java
+++ src/main/java/mainFile.java
@@ -1,11 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class mainFile {
 
+    static Connection c = null;
+    static String databaseURL;
+    static String databaseUsername;
+    static String databasePassword;
+
+    public static void setCredentials() throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("credentials.txt"));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            list.add(line);
+        }
+
+        if(list.size() != 3) {
+            System.out.println("3 Credentials required, please check credentials.txt for inputs. Exiting program!");
+            System.exit(1);
+        }
+
+        databaseURL = list.get(0);
+        databaseUsername = list.get(1);
+        databasePassword = list.get(2);
+    }
+
     public static void main(String[] args) {
 
-        ChannelThread t1 = new ChannelThread("Wynnsanity");
-        ChannelThread t2 = new ChannelThread("SnaxGaming");
-        new Thread(t1).start();
-        new Thread(t2).start();
+        try {
+            setCredentials();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:mysql://"+databaseURL,
+                            databaseUsername,databasePassword);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String [] channels = {"Wynnsanity","DesiArmyy","satvik"};
+        ExecutorService service = Executors.newFixedThreadPool(channels.length);
+
+        for (String s : channels) {
+            service.submit(new ChannelThread(s,c));
+        }
     }
 }
